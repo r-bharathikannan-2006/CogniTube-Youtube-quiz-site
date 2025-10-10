b552927ac551a6d4428d162444eed666e410cf5c
Bharathikannan R <r.bharathikannan.official@gmail.com>
2025-10-10 18:48:00 +0530

    Update apis.py

--- core/apis.py
+++ core/apis.py
@@ -104,7 +104,7 @@ def generate_questions(video_id):
     }
     # Question Generator
     prompt = f"""
-        Generate 15 technical or academical (concept based) multiple choice quiz questions based on the following transcript:
+        Generate 15 technical or academical (concept oriented) multiple choice quiz questions based on the following transcript:
 
         "{str(full_transript)}"
 
